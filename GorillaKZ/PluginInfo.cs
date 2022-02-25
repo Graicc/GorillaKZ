dee900e12bd45669de3f40eae69a5f312cba2a40
Graic <33105645+Graicc@users.noreply.github.com>
2022-02-24 21:33:56 -0500

    Bump version

--- GorillaKZ/PluginInfo.cs
+++ GorillaKZ/PluginInfo.cs
@@ -4,6 +4,6 @@
 	{
 		public const string GUID = "io.github.graicc.gorillatag.gorillakz";
 		public const string Name = "Gorilla KZ";
-		public const string Version = "1.0.0";
+		public const string Version = "0.1.0";
 	}
 }
